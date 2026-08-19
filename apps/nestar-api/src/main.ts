7182838a51efe6693e42ec84f944ac49d8257a21
MARCO <jmukhammadsodikh@gmail.com>
2026-08-19 16:26:21 +0900

    fix: global pipe validation

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 // negizdan boshlash bootstraping
 async function bootstrap() { // AppModule markaziy module
   const app = await NestFactory.create(AppModule); // Express + NestJs qorishmasi
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT_API ?? 3000);
 }
+
 bootstrap();
